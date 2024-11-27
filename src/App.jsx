5033ca1cfb3b73c4157ec2c7fe55bcae13c2cdbf
QzUzAzN <mailemisoquan@gmail.com>
2024-11-27 19:53:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <main className="overflow-x-hidden">
+        <Navbar />
+      </main>
+    </>
+  );
+}
+
+export default App;
